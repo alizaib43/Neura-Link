d9ccabd57bc57c09eccd54b29b9ca7c56a4a6f46
alizaib43 <zaib.hmatrix670@gmali.com>
2026-03-05 21:47:14 +0500

    Fix deployment workflow and cleanup next config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // basePath should match your repository name
   basePath: '/Neura-Link',
-  assetPrefix: '/Neura-Link/',
   images: {
     unoptimized: true,
   },
